ae33eefee10a1be5a9e637abc6e92fe74de2a6c1
diana asaftei <nicoleta.diana.22@gmail.com>
2020-11-28 15:52:49 +0200

    Add project files.

---
+++ Data/Asaftei_Nicoleta_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asaftei_Nicoleta_Lab8.Models;
+
+namespace Asaftei_Nicoleta_Lab8.Data
+{
+    public class Asaftei_Nicoleta_Lab8Context : DbContext
+    {
+        public Asaftei_Nicoleta_Lab8Context (DbContextOptions<Asaftei_Nicoleta_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asaftei_Nicoleta_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Asaftei_Nicoleta_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asaftei_Nicoleta_Lab8.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string PublisherName { get; set;}
+        public ICollection<Book> Books { get; set; }
+    }
+}
